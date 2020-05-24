e98d3e372612d030bbcac84e5d743be40bc9221f
VFryzlova <55710895+VFryzlova@users.noreply.github.com>
2020-05-24 11:20:18 +0200

    Přidány funkce pro zjištění, zda má každá z holek naspořeno dostatek a kolik jim zbývá naspořit

--- index.js
+++ index.js
@@ -11,6 +11,28 @@ let osoba2 = {
 
 let suma = osoba1.uspory + osoba2.uspory;
 
-if(suma >= 100000){
+const muzouJet = (suma) =>{
+  if(suma >= 100000){
   console.log("Holky můžou na dovolenou")
 }
+};
+
+let kontoAlena = osoba1.uspory;
+const zbyvaAlena = (kontoAlena) =>{
+  if(kontoAlena < 50000){
+  let zbyva = 50000 - kontoAlena;
+  console.log("Alena nemá naspořenou svou polovinu a chybí jí ještě " + zbyva)
+}else{
+  console.log("Alena má naspořenou svou polovinu")
+}
+};
+
+let kontoKarolina = osoba2.uspory;
+const zbyvaKarolina = (kontoKarolina) =>{
+  if(kontoKarolina < 50000){
+  let zbyva = 50000 - kontoKarolina;
+  console.log("Karolína nemá naspořenou svou polovinu a chybí jí ještě " + zbyva)
+}else{
+  console.log("Karolína má naspořenou svou polovinu")
+}
+};
