a0dbfbdd7c204da02af6cfafa845811f7597261f
VFryzlova <55710895+VFryzlova@users.noreply.github.com>
2020-05-24 11:35:13 +0200

    Upravena funkce muzouJet, přidán výpis, kolik jednotlivým holkám zbývá do své poloviny

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 let osoba1 = {
 	jmeno: 'Alena',
-	uspory: 53000
+	uspory: 23000
 };
 let osoba2 = {
 	jmeno: 'Karolína',
@@ -10,29 +10,33 @@ let osoba2 = {
 };
 
 let suma = osoba1.uspory + osoba2.uspory;
+let kontoAlena = osoba1.uspory;
+let kontoKarolina = osoba2.uspory;
+let Alenazbyva = 50000 - kontoAlena;
+let KarolinaZbyva = 50000 - kontoKarolina;
 
 const muzouJet = (suma) =>{
   if(suma >= 100000){
-  console.log("Holky můžou na dovolenou")
-}
+    console.log("Holky můžou na dovolenou")
+  }else{
+    console.log("Holky nemůžou jet na dovolenou, nemají dohromady 100 tisíc. Aleně chybí " + Alenazbyva + ", Karolíně zbývá " + KarolinaZbyva)
+  }
 };
 
-let kontoAlena = osoba1.uspory;
 const zbyvaAlena = (kontoAlena) =>{
   if(kontoAlena < 50000){
-  let zbyva = 50000 - kontoAlena;
-  console.log("Alena nemá naspořenou svou polovinu a chybí jí ještě " + zbyva)
-}else{
-  console.log("Alena má naspořenou svou polovinu")
-}
+    let Alenazbyva = 50000 - kontoAlena;
+    console.log("Alena nemá naspořenou svou polovinu a chybí jí ještě " + Alenazbyva)
+  }else{
+    console.log("Alena má naspořenou svou polovinu")
+  }
 };
 
-let kontoKarolina = osoba2.uspory;
 const zbyvaKarolina = (kontoKarolina) =>{
   if(kontoKarolina < 50000){
-  let zbyva = 50000 - kontoKarolina;
-  console.log("Karolína nemá naspořenou svou polovinu a chybí jí ještě " + zbyva)
-}else{
-  console.log("Karolína má naspořenou svou polovinu")
-}
+    let KarolinaZbyva = 50000 - kontoKarolina;
+    console.log("Karolína nemá naspořenou svou polovinu a chybí jí ještě " + KarolinaZbyva)
+  }else{
+    console.log("Karolína má naspořenou svou polovinu")
+  }
 };
