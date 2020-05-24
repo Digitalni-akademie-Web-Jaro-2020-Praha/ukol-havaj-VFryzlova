6624b3d20042f5ec323e6f3b3f5c028221ff3bea
VFryzlova <55710895+VFryzlova@users.noreply.github.com>
2020-05-24 10:55:41 +0200

    Zjištění, jestli holky mohou na dovolenou jet

---
+++ index.js
@@ -0,0 +1,16 @@
+"use strict";
+
+let osoba1 = {
+	jmeno: 'Alena',
+	uspory: 53000
+};
+let osoba2 = {
+	jmeno: 'Karolína',
+	uspory: 68000
+};
+
+let suma = osoba1.uspory + osoba2.uspory;
+
+if(suma >= 100000){
+  console.log("Holky můžou na dovolenou")
+}
